6464d766918a1ad7fba70c9c3f68dbd9b4aa9845
KhalilAbbaass <khaleel.abbaass@gmail.com>
2023-02-16 15:15:05 +0200

    login + jwt

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,15 +1,40 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React, { useState } from 'react'
+import { Link, useNavigate } from 'react-router-dom'
+import { loginUser } from '../services/Login'
 
 const Login = () => {
+  
+  const [inputs, setInputs] = useState({
+    username:"",
+    password:""
+  })
+
+  const [err,setErr] = useState(null)
+
+  const navigate = useNavigate()
+  
+  const handleChange = e => {
+    setInputs(prev=>({...prev, [e.target.name]: e.target.value}))
+  }
+
+  const handleSubmit = async e => {
+    e.preventDefault();
+   await  loginUser(inputs).then((result) => {
+      navigate('/')
+    }).catch((err) => {
+      setErr(err.response.data)
+    })
+
+  }
+
   return (
     <div className='loginDiv'>
       <h1>Login</h1>
       <form>
-        <input required type='text' placeholder='username'/>
-        <input required type='password' placeholder='password'/>
-        <button>Login</button>
-        <p>This is an error</p>
+        <input required type='text'  placeholder='username' name='username' onChange={handleChange}/>
+        <input required type='password' placeholder='password' name='password' onChange={handleChange}/>
+        <button onClick={handleSubmit}>Login</button>
+        {err && <p>{err}</p>}
         <span>Dont Have an Account ? <Link to='/register'>Register</Link></span>
       </form>
 
--- server/index.js
+++ server/index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const cookieParser = require('cookie-parser');
 
 const authRoute = require("./routes/auth");
 const postRoute = require("./routes/posts");
@@ -6,6 +7,7 @@ const userRoute = require("./routes/user");
 
 const app = express();
 app.use(express.json());
+app.use(cookieParser());
 
 app.use("/api/posts" , postRoute);
 app.use("/api/auth" , authRoute);
