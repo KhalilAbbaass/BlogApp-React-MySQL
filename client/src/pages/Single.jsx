58b30d0c3f8b0eaa3de87d6b79a10b5c7b76bdef
KhalilAbbaass <khaleel.abbaass@gmail.com>
2023-02-18 14:02:53 +0200

    single page and delete

--- client/src/pages/Single.jsx
+++ client/src/pages/Single.jsx
@@ -1,37 +1,70 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React, { useContext, useEffect, useState } from 'react';
+import { Link, useLocation, useNavigate } from 'react-router-dom';
 import Delete from '../Assets/delete.jpg';
 import Edit from '../Assets/edit.jpg';
 import Menu from '../components/Menu';
+import { getPost } from '../services/getPost';
+import moment from 'moment';
+import { AuthContext } from '../Context/authContext';
+import personLogo from '../Assets/personlogo.png';
+import { deletePost } from '../services/deletePost';
+
 
 const Single = () => {
+
+  const {currentUser} = useContext(AuthContext);
+
+  const [post , setPost] = useState({});
+
+  const location = useLocation()
+
+  const navigate = useNavigate();
+
+
+//splitting the url so that we can get the post id 
+  const postId = location.pathname.split("/")[2]
+
+  useEffect(() => {
+    const fetchData = async () =>{
+      getPost(postId).then((result) => {
+        setPost(result.data);
+
+      }).catch((err) => {
+        console.log(err)
+      })
+    };
+    fetchData();
+  } ,[postId])
+
+  const handleDelete = async () => {
+    deletePost(postId).then((result) => {
+      navigate("/")
+
+    }).catch((err) => {
+      console.log(err)
+    })
+  }
+
   return (
     <div className='single'>
 
       <div className='content'>
-        <img src='https://images.pexels.com/photos/6489663/pexels-photo-6489663.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2' alt=''/>
+        <img src={post?.img} alt=''/>
          <div className="user">
-          <img alt='' src='https://static.ffx.io/images/$zoom_2.3309%2C$multiply_0.5855%2C$ratio_1%2C$width_1059%2C$x_678%2C$y_162/t_crop_custom/q_86%2Cf_auto/89dcd9ed59d16f7c77f001daa35c38e30b16c8dc'/>
+          <img alt='' src={post?.userImg ? post.userImg : personLogo}/>
           <div className="info">
-              <span>Andrew</span>
-              <p>Posted 2 days ago</p>
+              <span>{post.username}</span>
+              <p>Posted {moment(post.date).fromNow()}</p>
           </div> 
-          <div className="edit">
-            <Link to='/write?edit=2'><img src={Edit} alt=''/></Link>
-            
-            <img src={Delete} alt=''/>
-          </div>
+       {currentUser?.username === post.username && 
+            <div className="edit">
+            <Link to='/write?edit=2'><img src={Edit} alt='' /></Link>
+            <img src={Delete} alt='' onClick={handleDelete}/>
+          </div>}
         </div>
-          <h1>This is a title</h1>
+          <h1>{post.title}</h1>
           <p>
-            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Necessitatibus, nihil labore. Optio labore illum saepe nulla magnam earum modi laboriosam ducimus similique, rem aspernatur, quo, deserunt nobis iusto eos unde. 
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Modi, culpa sequi magni voluptate reprehenderit aperiam cum repellat quisquam vel saepe aut commodi qui nostrum deserunt a blanditiis nesciunt tenetur ut!
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Obcaecati debitis tenetur saepe cupiditate nulla ex praesentium aliquid non aut placeat autem fugiat, nobis totam iure laudantium suscipit soluta? Explicabo, odio!
-            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ipsa quibusdam iure cumque dignissimos veritatis atque, voluptates aut maiores aspernatur perspiciatis temporibus deleniti laboriosam, architecto sed ex eum neque? Odit, tempora.
-            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Necessitatibus, nihil labore. Optio labore illum saepe nulla magnam earum modi laboriosam ducimus similique, rem aspernatur, quo, deserunt nobis iusto eos unde. 
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Modi, culpa sequi magni voluptate reprehenderit aperiam cum repellat quisquam vel saepe aut commodi qui nostrum deserunt a blanditiis nesciunt tenetur ut!
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Obcaecati debitis tenetur saepe cupiditate nulla ex praesentium aliquid non aut placeat autem fugiat, nobis totam iure laudantium suscipit soluta? Explicabo, odio!
-            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ipsa quibusdam iure cumque dignissimos veritatis atque, voluptates aut maiores aspernatur perspiciatis temporibus deleniti laboriosam, architecto sed ex eum neque? Odit, tempora.
+              {post.desc}
           </p>
       </div>
    
--- server/controllers/posts.js
+++ server/controllers/posts.js
@@ -1,17 +1,24 @@
 const { db } = require("../db");
+const jwt = require("jsonwebtoken")
 
 const getPosts = (req , res) => {
     const q = req.query.cat ? "SELECT * FROM posts WHERE cat=?" : "SELECT * FROM posts";
 
     db.query(q, [req.query.cat], (err, data) => {
-        if(err) return res.send(err);
+        if(err) return res.status(500).send(err);
 
         return res.status(200).json(data);
     });
 }
 
 const getPost = (req , res) => {
-    res.json("Hi")
+   const q = "SELECT `username`, `title` , `desc`, p.img ,u.img AS userImg, `cat` , `date` FROM users u JOIN posts p ON u.id=p.uid WHERE p.id = ?"
+
+   db.query(q, [req.params.id] , (err, data) => {
+    if(err) return res.status(500).json(err)
+
+    return res.status(200).json(data[0])
+   })
 }
 
 const addPost = (req , res) => {
@@ -19,7 +26,21 @@ const addPost = (req , res) => {
 }
 
 const deletePost = (req , res) => {
-    res.json("Hi")
+   const token  = req.cookies.access_token
+   if(!token) return res.status(401).json("Not Authenticated")
+
+   jwt.verify(token , "jwtkey" , (err , userInfo) => {
+    if(err) return res.status(403).json("token is not valid")
+
+    const postId = req.params.id
+    const q = "DELETE FROM posts WHERE `id` = ? AND `uid` = ?"
+
+    db.query(q, [postId , userInfo.id] , (err, data) => {
+        if(err) return res.status(403).json("you cant delete a post that doesnt belong to you")
+        return res.json("Post has been deleted!")
+    })
+   })
+
 }
 
 const updatePost = (req , res) => {
